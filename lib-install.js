d523bd11eb0a155e059392d60e69e80d161a7803
Leviathan <86807700+Leviathenn@users.noreply.github.com>
2023-12-22 22:42:30 -0500

    Add files via upload

---
+++ lib-install.js
@@ -0,0 +1,95 @@
+/**
+ * @author Leviathen
+ */
+const fs = require('fs-extra');
+const JSZip = require("jszip");
+const axios = require("axios").default;
+const luainjs = require('lua-in-js');
+const { exit } = require("process");
+const extract = require('extract-zip')
+const path = require("path");
+const luaEnv = luainjs.createEnv({
+    fileExists: p => fs.existsSync(p),
+    loadFile: p => fs.readFileSync(p, { encoding: 'utf8' }),
+    osExit: code => (exitCode += code)   // function called by os.exit
+}) 
+var hasRanComp = false;
+const lib = {
+    installLibraries: (config)=>{
+        if(!hasRanComp){
+          //console.log(config.strValues)
+          if(config.strValues["requireRoot"]){
+            if(isRoot()){
+             
+            }else{
+              console.log("This build requires root."); 
+              exit(1);
+            }
+          }
+          (async ()=>{
+            try {
+              if(fs.existsSync(`/tmp/cache`)){
+                fs.rmSync(`/tmp/cache`,{recursive:true, force:true})
+              }
+              await extract(`${process.cwd()}/libinst.dat`, { dir: '/tmp/cache' })
+          
+              fs.readdirSync(`/tmp/cache`).forEach((fDir)=>{
+           
+                if(!config.strValues["extractPath"]["strValues"][fDir]){
+                  
+                }else{
+                  fs.copySync(`/tmp/cache/${fDir}`,`${config.strValues["extractPath"]["strValues"][fDir]}`)
+                }
+              })
+              console.log('Extraction complete')
+            } catch (err) {
+              // handle any errors  
+            }
+          })();
+        }else{
+          
+        }
+      
+    }
+    
+}
+/**
+ * @summary this function compresses the files when called by weld that was specified in the "Stick.json" file.
+ */
+function isRoot() {
+  return !process.getuid(); // getuid() returns 0 for root
+}
+
+function addFolderToZip(zip, folderPath) {
+  const files = fs.readdirSync(folderPath);
+
+  files.forEach((file) => {
+      const filePath = path.join(folderPath, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+          addFolderToZip(zip.folder(file), filePath);
+      } else if (stat.isFile()) {
+          
+          zip.file(file, fs.readFileSync(filePath));
+      }
+  });
+}
+
+function compile(funcArg){
+  hasRanComp = true;
+  let compileDirectory = `${funcArg}/compile`
+  fs.readdirSync(compileDirectory).forEach((drinf)=>{
+    
+    var zip = new JSZip();
+    addFolderToZip(zip, `${funcArg}/compile`)
+    zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+    .pipe(fs.createWriteStream(`${funcArg}/libinst.dat`))
+    .on('finish', function () {
+        console.log("Finished Compiling! Run without compile to run your full code.")
+    });
+  })
+}
+exports.compile = compile;
+exports.lib = lib;
+
